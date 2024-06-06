d7c277e36761fe2bedf3e001d4644c7e41bb7f8a
Kovr1k <s.mosiencko@gmail.com>
2024-06-06 12:07:34 +0300

    Тесты

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+class SimpleTests(TestCase):
+    def setUp(self):
+        # Настройка контекста для тестов
+        User.objects.create(username='Test', password='123456')
+
+    def test_home_page_status_code(self):
+        # Тест на доступность главной страницы
+        response = self.client.get('/', follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        # Тест на использование правильного шаблона
+        response = self.client.get('/books/createBook/')
+        self.assertTemplateUsed(response, 'main/createBook.html')
+
+    def test_home_page_does_not_contain_incorrect_html(self):
+        # Тест на отсутствие неправильного HTML на главной странице
+        response = self.client.get('/New/', follow=True)
+        self.assertNotContains(response, 'Пидор')
